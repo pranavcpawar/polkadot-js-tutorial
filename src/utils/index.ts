bc14fbd20adfc09b2c2439c72780003f2a7083f3
pranavcpawar <pranavcpawar@outlook.com>
2024-10-19 09:24:51 +0530

    function to truncate address

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,3 +1,8 @@
 export function classNames(...classes: unknown[]): string {
   return classes.filter(Boolean).join(' ')
 }
+
+export function truncate(_walletAddress: string | undefined): string {
+  if (!_walletAddress) return ''
+  return _walletAddress.substring(0, 6) + '...' + _walletAddress.slice(-4)
+}
