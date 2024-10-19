9229280600f99830ebf876402b191c9d78462315
pranavcpawar <pranavcpawar@outlook.com>
2024-10-19 09:29:13 +0530

    final dapp

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,45 @@
+import { usePolkadot } from 'hooks/usePolkadot'
+import Header from './Header'
+import AccountInfo from './Account'
+import GetRPC from './GetRPC'
+import Transfer from './Transfer'
+import { Toaster } from 'sonner'
+import Sign from './Sign'
+
 function App() {
+  // TODO
+  const { rpc, setRpc, pubKey, setPubKey, isRPC, setIsRPC } = usePolkadot()
+
   return (
-    <div className="relative overflow-hidden">
-      <div className="h-screen sm:pb-40 sm:pt-24 lg:pb-48 lg:pt-40"></div>
+    <div className="relative flex min-h-screen w-full flex-col items-center justify-center space-y-4 overflow-hidden p-3">
+      <Toaster position="top-center" toastOptions={{ duration: 3000 }} />
+      <Header />
+      <div className="flex flex-1 flex-col place-items-start items-center justify-start gap-4">
+        <h3 className="font-unbounded text-lg tracking-tight text-white sm:text-xl">
+          Polkadot-JS API Tutorial
+        </h3>
+      </div>
+      <Sign />
+      <Transfer />
+      <div className="flex flex-1 flex-col place-items-start items-center justify-start gap-4">
+        {isRPC ? (
+          <AccountInfo
+            rpc={rpc}
+            isRPC={isRPC}
+            setIsRPC={setIsRPC}
+            setRpc={setRpc}
+          />
+        ) : (
+          <GetRPC
+            isRPC={isRPC}
+            setIsRPC={setIsRPC}
+            setPubKey={setPubKey}
+            pubKey={pubKey}
+            setRpc={setRpc}
+            rpc={rpc}
+          />
+        )}
+      </div>
     </div>
   )
 }
