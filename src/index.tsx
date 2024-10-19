1ffaff1d8599b9dff0d5d186fa7096e01d933977
pranavcpawar <pranavcpawar@outlook.com>
2024-10-19 09:24:20 +0530

    wrap dapp with polkadot provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,13 @@
 import { createRoot } from 'react-dom/client'
 import 'tailwindcss/tailwind.css'
 import App from 'components/App'
+import { PolkadotProvider } from 'hooks/usePolkadot'
 
 const container = document.getElementById('root') as HTMLDivElement
 const root = createRoot(container)
 
-root.render(<App />)
+root.render(
+  <PolkadotProvider>
+    <App />
+  </PolkadotProvider>
+)
